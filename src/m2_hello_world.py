8d47e51bf95e133686a771ee640881b4fd0e3fcb
danielcr <danielcr@rose-hulman.edu>
2017-11-27 10:31:50 -0500

    m2 done

--- src/m2_hello_world.py
+++ src/m2_hello_world.py
@@ -26,7 +26,7 @@ print('Hello, World')
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Notice the small horizontal BLUE bars on the scrollbar-like thing
 #   on the right.  Each blue bar indicates a TO DO in this module.
 #
@@ -51,7 +51,7 @@ print('Hello, World')
 
 ########################################################################
 #
-# TODO: 3.
+# Done: 3.
 #   Add another   print   statement below.
 #   It should print any string that you want (but keep it G-rated!)
 #   Test your code by re-running this module using either the right click
@@ -59,10 +59,10 @@ print('Hello, World')
 #   Look at the Console to be sure that your string printed as expected.
 #
 ########################################################################
-
+print('test')
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add yet another   print   statement.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
@@ -73,7 +73,7 @@ print('Hello, World')
 #   (HINT: It is an INTERESTING number.)  Get help if your value is wrong.
 #
 ########################################################################
-
+print(2*2)
 ########################################################################
 #
 # TODO: 5.
